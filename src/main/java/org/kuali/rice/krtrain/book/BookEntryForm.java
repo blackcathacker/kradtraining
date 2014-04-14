211485ce5dabfd0dc3c378709146056fa730d9d0
blackcathacker <douglas.pace@gmail.com>
2014-04-14 15:28:08 -0400

    exercise8

---
+++ src/main/java/org/kuali/rice/krtrain/book/BookEntryForm.java
@@ -0,0 +1,17 @@
+package org.kuali.rice.krtrain.book;
+
+import org.kuali.rice.krad.web.form.UifFormBase;
+
+public class BookEntryForm extends UifFormBase {
+
+	private Book book;
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
+}
